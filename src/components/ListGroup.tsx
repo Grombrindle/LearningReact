a9219c0260e70165041fcc1e0958041a58767e07
Grombrindle <everythingiswronj@gmail.com>
2026-03-21 19:40:54 +0300

    Conditional Rendering and some shinanigins

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,46 @@
+// import { Fragment } from "react/jsx-runtime";
 function ListGroup() {
+  let items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+
+  items = [];
+
+  //   if (items.length === 0)
+  //     return (
+  //       <>
+  //         <h1>List Group</h1>
+  //         <p>There are no items in the list.</p>
+  //       </>
+  //     );
+
+  //   items.map((item) => <li className="list-group-item">{item}</li>);
+
+  //   const message =
+  //     items.length === 0 ? <p>There are no items in the list.</p> : null;
+  function GetMessage() {
+    if (items.length === 0) return <p>There are no items in the list.</p>;
+    return null;
+  }
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    // <Fragment>
+    <>
+      <h1>List Group</h1>
+      {/* {GetMessage()} */}
+      {items.length === 0 && <p>There are no items in the list.</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+    // </Fragment>
   );
 }
 
