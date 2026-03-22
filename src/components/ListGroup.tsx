d4baa4dc29f4cb0a7650ada447828dee0a965748
Grombrindle <everythingiswronj@gmail.com>
2026-03-22 09:53:20 +0300

    managing states and hooks

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,6 @@
-import type { MouseEvent } from "react";
+// import type { MouseEvent } from "react";
+
+import { useState } from "react";
 
 // import { Fragment } from "react/jsx-runtime";
 function ListGroup() {
@@ -9,7 +11,12 @@ function ListGroup() {
     "A fourth item",
     "And a fifth one",
   ];
+  // let selectedItem = 0;
 
+  //Hooks
+  const [selectedItem, setSelectedItem] = useState(-1);
+  // arr[0]; // -1
+  // arr[1]; // function to update the state
   //   items = [];
 
   //   if (items.length === 0)
@@ -24,12 +31,13 @@ function ListGroup() {
 
   //   const message =
   //     items.length === 0 ? <p>There are no items in the list.</p> : null;
-  function GetMessage() {
-    if (items.length === 0) return <p>There are no items in the list.</p>;
-    return null;
-  }
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // function GetMessage() {
+  //   if (items.length === 0) return <p>There are no items in the list.</p>;
+  //   return null;
+  // }
+
+  // const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     // <Fragment>
@@ -39,7 +47,15 @@ function ListGroup() {
       {items.length === 0 && <p>There are no items in the list.</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedItem === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedItem(index)}
+          >
             {item}
           </li>
         ))}
